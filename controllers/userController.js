eaf93fd45ac5041e2a30ba61deef69c728dc8916
Maryam Fadaee <maryamfadaee@gmail.com>
2025-05-02 11:50:56 -0400

    create api for login user

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,25 @@ const createToken = (id) => {
 };
 
 // Route for user login
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+	try {
+		const { email, password } = req.body;
+		const user = await userModel.findOne({ email });
+		if (!user) {
+			res.json({ success: true, message: "User does not exists" });
+		}
+		const isMatch = await bcrypt.compare(password, user.password);
+		if (isMatch) {
+			const token = createToken(user._id);
+			res.json({ success: true, token });
+		} else {
+			res.json({ success: false, message: "Invalid credentials" });
+		}
+	} catch (error) {
+		console.log(error, "error in login user");
+		res.json({ success: false, message: error.message });
+	}
+};
 
 //Route for user registration
 const registerUser = async (req, res) => {
