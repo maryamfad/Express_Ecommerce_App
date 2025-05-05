de432b2934fa91980601801ae0d918fc49d18931
Maryam Fadaee <maryamfadaee@gmail.com>
2025-05-05 18:00:18 -0400

    implement add to cart functionality

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,5 +1,29 @@
+import userModel from "../models/userModel";
 // add products to user cart
-const addToCart = async (req, res) => {};
+const addToCart = async (req, res) => {
+	const { userId, itemId, size } = req.body;
+	try {
+		const userData = await userModel.findById(userId);
+		const cartData = await userData.cartData;
+
+		if (cartData[itemId]) {
+			if (cartData[itemId][size]) {
+				cartData[itemId][size] += 1;
+			} else {
+				cartData[itemId][size] = 1;
+			}
+		} else {
+			cartData[itemId] = {};
+			cartData[itemId][size] = 1;
+		}
+
+		await userModel.findByIdAndUpdate(userId, { cartData });
+		res.json({ success: true, message: "Added TO Cart." });
+	} catch (error) {
+		console.log(error);
+		res.json({ success: false, message: error.message });
+	}
+};
 
 // update user cart
 const updateCart = async (req, res) => {};
