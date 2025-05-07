92bdf8e570fb9aa356cc703f9a4f33f7ad8fba80
Maryam Fadaee <maryamfadaee@gmail.com>
2025-05-07 09:48:29 -0400

    create orderController

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,1 +1,26 @@
+//placing orders using COD Method
+const placeOrder = (async = (req, res) => {});
 
+//placing orders using Stripe Method
+const placeOrderStripe = (async = (req, res) => {});
+
+//placing orders using Razorpay Method
+const placeOrderRazorpay = (async = (req, res) => {});
+
+//All Orders data for Admin Panel
+const allOrders = async (req, res) => {};
+
+//User order data for frontend
+const userOrders = async (req, res) => {};
+
+//update order status from Admin Panel
+const updateStatus = async (req, res) => {};
+
+export {
+	placeOrder,
+	placeOrderRazorpay,
+	placeOrderStripe,
+	allOrders,
+	userOrders,
+	updateStatus,
+};
