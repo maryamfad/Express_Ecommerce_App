35174b92080d43b13f95a22ce5dddafe7d8f039c
Maryam Fadaee <maryamfadaee@gmail.com>
2025-05-07 15:40:50 -0400

    create functionality for stripe payment

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,5 +1,13 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
+import Stripe from "stripe";
+
+//global variables
+const currency = "csd";
+const deliveryCharges = 10;
+
+//gateway initialize
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //placing orders using COD Method
 const placeOrder = async (req, res) => {
 	try {
@@ -27,7 +35,55 @@ const placeOrder = async (req, res) => {
 };
 
 //placing orders using Stripe Method
-const placeOrderStripe = async (req, res) => {};
+const placeOrderStripe = async (req, res) => {
+	try {
+		const { userId, items, amount, address } = req.body;
+		const { origin } = req.headers;
+		const orderData = {
+			userId,
+			items,
+			address,
+			amount,
+			paymentMethod: "Stripe",
+			payment: false,
+			date: Date.now(),
+		};
+		const newOrder = new orderModel(orderData);
+		await newOrder.save();
+
+		const line_items = items.map((item) => ({
+			price_data: {
+				currency: currency,
+				product_data: {
+					name: item.name,
+				},
+				unit_amount: item.price * 100,
+			},
+			quantity: item.quantity,
+		}));
+		line_items.push({
+			price_data: {
+				currency: currency,
+				product_data: {
+					name: "Delivery Charges",
+				},
+				unit_amount: deliveryCharges * 100,
+			},
+			quantity: 1,
+		});
+		const session = await stripe.checkout.sessions.create({
+			success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+			cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+			line_items,
+			mode: "payment",
+		});
+
+		res.json({ success: true, session_url: session.url });
+	} catch (error) {
+		console.log(error);
+		res.json({ success: false, message: error.message });
+	}
+};
 
 //placing orders using Razorpay Method
 const placeOrderRazorpay = async (req, res) => {};
