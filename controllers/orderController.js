f547688236ccf9f302de7325df15300e0918b647
Maryam Fadaee <maryamfadaee@gmail.com>
2025-05-07 11:56:50 -0400

    complete allOrder endpoint for admin panel to get all the orders

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -33,13 +33,21 @@ const placeOrderStripe = async (req, res) => {};
 const placeOrderRazorpay = async (req, res) => {};
 
 //All Orders data for Admin Panel
-const allOrders = async (req, res) => {};
+const allOrders = async (req, res) => {
+	try {
+		const orders = await orderModel.find({});
+		res.json({ success: true, orders });
+	} catch (error) {
+		console.log(error);
+		res.json({ success: false, message: error.message });
+	}
+};
 
 //User order data for frontend
 const userOrders = async (req, res) => {
 	try {
 		const { userId } = req.body;
-		orders = await orderModel.find({ userId });
+		const orders = await orderModel.find({ userId });
 		res.json({ success: true, orders });
 	} catch (error) {
 		console.log(error);
